f135a9d0a797bb4c237452cde67a2c806865d2dd
Tony Trieu <uytuan213@gmail.com>
2020-10-25 12:33:53 -0400

    Updated nuGet packages & Added RatingsController and ReviewsController

---
+++ TheWizardsGameShop/Controllers/RatingsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheWizardsGameShop.Models;
+
+namespace TheWizardsGameShop.Controllers
+{
+    public class RatingsController : Controller
+    {
+        private readonly TheWizardsGameShopContext _context;
+
+        public RatingsController(TheWizardsGameShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ratings
+        public async Task<IActionResult> Index()
+        {
+            var theWizardsGameShopContext = _context.Rating.Include(r => r.Game).Include(r => r.User);
+            return View(await theWizardsGameShopContext.ToListAsync());
+        }
+
+        // GET: Ratings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating
+                .Include(r => r.Game)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.RatingId == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // GET: Ratings/Create
+        public IActionResult Create()
+        {
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath");
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email");
+            return View();
+        }
+
+        // POST: Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RatingId,Rate,UserId,GameId")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", rating.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", rating.UserId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", rating.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", rating.UserId);
+            return View(rating);
+        }
+
+        // POST: Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RatingId,Rate,UserId,GameId")] Rating rating)
+        {
+            if (id != rating.RatingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RatingExists(rating.RatingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", rating.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", rating.UserId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating
+                .Include(r => r.Game)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.RatingId == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // POST: Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rating = await _context.Rating.FindAsync(id);
+            _context.Rating.Remove(rating);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RatingExists(int id)
+        {
+            return _context.Rating.Any(e => e.RatingId == id);
+        }
+    }
+}
---
+++ TheWizardsGameShop/Controllers/ReviewsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheWizardsGameShop.Models;
+
+namespace TheWizardsGameShop.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly TheWizardsGameShopContext _context;
+
+        public ReviewsController(TheWizardsGameShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            var theWizardsGameShopContext = _context.Review.Include(r => r.Game).Include(r => r.User);
+            return View(await theWizardsGameShopContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.Game)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.ReviewId == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath");
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReviewId,ReviewContent,UserId,GameId,ReviewDate,IsPublished")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", review.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", review.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", review.UserId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReviewId,ReviewContent,UserId,GameId,ReviewDate,IsPublished")] Review review)
+        {
+            if (id != review.ReviewId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.ReviewId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GameId"] = new SelectList(_context.Game, "GameId", "GameDigitalPath", review.GameId);
+            ViewData["UserId"] = new SelectList(_context.WizardsUser, "UserId", "Email", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.Game)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.ReviewId == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Review.FindAsync(id);
+            _context.Review.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Review.Any(e => e.ReviewId == id);
+        }
+    }
+}
